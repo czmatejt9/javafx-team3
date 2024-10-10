067f6aa853765811baf552c4cf0bfd1367b7255b
Rustatano <jakub-svoboda111@seznam.cz>
2024-10-10 13:03:16 +0200

    edited .gitignore

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.javafx_team3 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
     opens com.javafx_team3 to javafx.fxml;
     exports com.javafx_team3;
